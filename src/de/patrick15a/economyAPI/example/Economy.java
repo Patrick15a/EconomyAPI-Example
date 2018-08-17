30704af653e6c9df3071c5f7baa338b87871d075
Patrick15a <gamer.patrick15@gmx.de>
2018-08-17 20:55:28 +0200

    Update Economy.java

--- src/de/patrick15a/economyAPI/example/Economy.java
+++ src/de/patrick15a/economyAPI/example/Economy.java
@@ -3,21 +3,30 @@ package de.patrick15a.economyAPI.example;
 import org.bukkit.entity.Player;
 
 public class Economy {
-
-	public void get(Player p) {
-		Main.getEconomyAPI().getMoney(p);
+	
+	// Gibt zurück wie viel Geld der Spieler hat.
+	public int get(Player p) {
+		return Main.getEconomyAPI().getMoney(p);
 	}
 	
-	public void add(Player p, int money) {
-		Main.getEconomyAPI().addMoney(p, money);
+	// Fügt dem Spieler Geld hinzu. Gibt zurück ob Funktion erfolgreich ausgeführt wurde.
+	public boolean add(Player p, int money) {
+		return Main.getEconomyAPI().addMoney(p, money);
 	}
 	
-	public void remove(Player p, int money) {
-		Main.getEconomyAPI().removeMoney(p, money);
+	// Entfernt dem Spieler Geld. Gibt zurück ob Funktion erfolgreich ausgeführt wurde.
+	public boolean remove(Player p, int money) {
+		return Main.getEconomyAPI().removeMoney(p, money);
 	}
 	
-	public void set(Player p, int money) {
-		Main.getEconomyAPI().setMoney(p, money);
+	// Setzt dem Spieler Geld. Gibt zurück ob Funktion erfolgreich ausgeführt wurde.
+	public boolean set(Player p, int money) {
+		return Main.getEconomyAPI().setMoney(p, money);
+	}
+	
+	// Gibt zurück ob der Spieler "amount" Geld hat.
+	public boolean has(Player p, int amount) {
+		return Main.getEconomyAPI().hasMoney(p, amount);
 	}
 	
 }
